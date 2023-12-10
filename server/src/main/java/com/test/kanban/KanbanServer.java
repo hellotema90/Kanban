b4fdf57efb71fec5844701625fdcabbdd3728f31
hellotema90 <as.masalkin@yandex.ru>
2023-12-11 00:40:52 +0300

    Первая версия

---
+++ server/src/main/java/com/test/kanban/KanbanServer.java
@@ -0,0 +1,12 @@
+package com.test.kanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class KanbanServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanServer.class, args);
+	}
+
+}
